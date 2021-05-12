fe611ca594853bb0d4e4f4bbdc583b12227c6c0d
andronedev <31452517+andronedev@users.noreply.github.com>
2021-05-12 17:53:26 +0200

    update

---
+++ EXEMPLE.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    client: {
+        token: 'token du bot',
+    },
+
+    dashboard: {
+        url: 'http://localhost',
+        port: 3000,
+        secret: 'secret of the app discord',
+    },
+
+    autoRedirect: false,
+
+    supportGuild: {
+        enabled: true,
+        urlCode: 'ygGQXhAn',
+    },
+    mongodb :{
+        url :"",
+        dbname : "megaweb"
+    },
+    salt:"random string" 
+
+};
+
+/*
+ https://discord.com/developers/applications pour obtenir le token du bot et le secret
+ Pour mongodb vous pouvez utiliser https://www.mongodb.com/cloud/atlas ou un serveur en local
+*/
